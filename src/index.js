d31f1970f94a2931b9cf7b2e38e1da92655aa34b
Trwitih Mutitakul <triwith.m@ku.th>
2018-03-15 02:33:46 +0700

    Add route example

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux';
+import {createStore , applyMiddleware} from 'redux';
+import {BrowserRouter,Route} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+class Hello extends React.Component {
+  render(){
+    return <div>Hello</div>
+  }
+}
+
+class Goodbye extends React.Component {
+  render(){
+    return <div>Good Bye</div>
+  }
+}
+
+
+ReactDOM.render((<BrowserRouter>
+  <div>
+    Headder
+    <Route path="/hello" component={Hello}/>
+    <Route path="/goodbye" component={Goodbye} />
+  </div>
+</BrowserRouter>)
+  ,document.querySelector('.root'));
