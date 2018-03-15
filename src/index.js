5e8123561e2e91db606f21f67266421581a790bf
Trwitih Mutitakul <triwith.m@ku.th>
2018-03-15 12:01:17 +0700

    Create simple postindex

--- src/index.js
+++ src/index.js
@@ -4,26 +4,14 @@ import {Provider} from 'react-redux';
 import {createStore , applyMiddleware} from 'redux';
 import {BrowserRouter,Route} from 'react-router-dom'
 
+import PostsIndex from './components/posts_index'
+
 const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-class Hello extends React.Component {
-  render(){
-    return <div>Hello</div>
-  }
-}
-
-class Goodbye extends React.Component {
-  render(){
-    return <div>Good Bye</div>
-  }
-}
 
 
 ReactDOM.render((<BrowserRouter>
   <div>
-    Headder
-    <Route path="/hello" component={Hello}/>
-    <Route path="/goodbye" component={Goodbye} />
+    <Route path="/" component={PostsIndex} />
   </div>
 </BrowserRouter>)
   ,document.querySelector('.root'));
