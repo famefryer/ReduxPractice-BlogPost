368292cf7ee6d3bf1d495f5514ee783ff1232388
Trwitih Mutitakul <triwith.m@ku.th>
2018-03-17 01:10:32 +0700

    Make post-list receive data

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const FETCH_POST = "FETCH_POST";
 const ROOTURL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key="FORFAME123"'
+const API_KEY = '?key=FORFAME123'
 export function fetchPost(){
   const request = axios.get(`${ROOTURL}/posts${API_KEY}`);
   return {
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,13 +1,33 @@
 import React , {Component} from 'react'
 import {connect} from 'react-redux'
+import {fetchPost} from '../actions'
+import _ from 'lodash'
 
 class PostsIndex extends Component {
+  componentDidMount(){
+    this.props.fetchPost();
+  }
+
+  renderPost(){
+    return _.map(this.props.posts, (post) => {
+      return (<li className="list-group-title" key={post.id}>
+        {post.title}
+      </li>
+      )
+    });
+  }
+
   render(){
-    return <div>Postsindex</div>
+    return (
+      <div>
+        <h3>Posts</h3>
+        <ul className="list-group">{this.renderPost()}</ul>
+      </div>)
   }
 }
 
 function mapStateToProps(state){
-  return {posts:state.props};
+  return {posts:state.posts}
 }
-export default connect(mapStateToProps)(PostsIndex);
+//like dispatch function
+export default connect(mapStateToProps,{fetchPost})(PostsIndex);
