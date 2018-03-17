ac7c6d99dc7d92fbaa3b8ff59ca30e52fa7bb192
Trwitih Mutitakul <triwith.m@ku.th>
2018-03-17 23:52:39 +0700

    Connect new and list page

---
+++ src/components/post_new.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+
+class PostNew extends Component {
+  render(){
+    return(
+      <div>
+        New post page
+      </div>
+    );
+  }
+}
+
+export default PostNew
