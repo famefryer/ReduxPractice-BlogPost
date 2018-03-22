c2dc1f56ae0e2d3474ed5c62f7f5e40376bc0767
Trwitih Mutitakul <triwith.m@ku.th>
2018-03-22 22:35:45 +0700

    Add cancel btn

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,17 +1,25 @@
 import React,{Component} from 'react'
 import {Field,reduxForm} from 'redux-form'
+import {Link} from 'react-router-dom'
+import '../style/style.css'
 
 class PostNew extends Component {
   renderField(field){
+
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`
+
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input
           className="form-control"
           type="text"
           {...field.input}
         />
-        {field.meta.error}
+      <p className="text-help">
+          {touched ? error : ''}
+        </p>
       </div>
     )
   }
@@ -40,9 +48,13 @@ class PostNew extends Component {
           component={this.renderField}
         />
         <button type="submit"
-        className="btn btn-primary">
-        Submit
+          className="btn btn-primary">
+          Submit
         </button>
+        <Link to="/"
+          className="btn btn-danger">
+          Cancel
+        </Link>
       </form>
     );
   }
